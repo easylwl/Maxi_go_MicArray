954fc151b9aef183051141250bb2bf3c8723e7db
easylwl <easylwl@163.com>
2019-10-27 22:17:02 +0800

    k210 apu deom build succeful

---
+++ build/lib/CMakeFiles/kendryte.dir/DependInfo.cmake
@@ -0,0 +1,65 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "J:/sipeed/k210/apu_test/lib/bsp/crt.S" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/bsp/crt.S.obj"
+  "J:/sipeed/k210/apu_test/lib/bsp/entry.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/bsp/entry.c.obj"
+  "J:/sipeed/k210/apu_test/lib/bsp/entry_user.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/bsp/entry_user.c.obj"
+  "J:/sipeed/k210/apu_test/lib/bsp/interrupt.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/bsp/interrupt.c.obj"
+  "J:/sipeed/k210/apu_test/lib/bsp/locks.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/bsp/locks.c.obj"
+  "J:/sipeed/k210/apu_test/lib/bsp/printf.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/bsp/printf.c.obj"
+  "J:/sipeed/k210/apu_test/lib/bsp/sleep.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/bsp/sleep.c.obj"
+  "J:/sipeed/k210/apu_test/lib/bsp/syscalls.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/bsp/syscalls.c.obj"
+  "J:/sipeed/k210/apu_test/lib/drivers/aes.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/drivers/aes.c.obj"
+  "J:/sipeed/k210/apu_test/lib/drivers/apu.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/drivers/apu.c.obj"
+  "J:/sipeed/k210/apu_test/lib/drivers/clint.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/drivers/clint.c.obj"
+  "J:/sipeed/k210/apu_test/lib/drivers/dmac.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/drivers/dmac.c.obj"
+  "J:/sipeed/k210/apu_test/lib/drivers/dvp.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/drivers/dvp.c.obj"
+  "J:/sipeed/k210/apu_test/lib/drivers/fft.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/drivers/fft.c.obj"
+  "J:/sipeed/k210/apu_test/lib/drivers/fpioa.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/drivers/fpioa.c.obj"
+  "J:/sipeed/k210/apu_test/lib/drivers/gpio.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/drivers/gpio.c.obj"
+  "J:/sipeed/k210/apu_test/lib/drivers/gpiohs.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/drivers/gpiohs.c.obj"
+  "J:/sipeed/k210/apu_test/lib/drivers/i2c.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/drivers/i2c.c.obj"
+  "J:/sipeed/k210/apu_test/lib/drivers/i2s.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/drivers/i2s.c.obj"
+  "J:/sipeed/k210/apu_test/lib/drivers/kpu.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/drivers/kpu.c.obj"
+  "J:/sipeed/k210/apu_test/lib/drivers/plic.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/drivers/plic.c.obj"
+  "J:/sipeed/k210/apu_test/lib/drivers/pwm.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/drivers/pwm.c.obj"
+  "J:/sipeed/k210/apu_test/lib/drivers/rtc.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/drivers/rtc.c.obj"
+  "J:/sipeed/k210/apu_test/lib/drivers/sha256.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/drivers/sha256.c.obj"
+  "J:/sipeed/k210/apu_test/lib/drivers/spi.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/drivers/spi.c.obj"
+  "J:/sipeed/k210/apu_test/lib/drivers/sysctl.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/drivers/sysctl.c.obj"
+  "J:/sipeed/k210/apu_test/lib/drivers/timer.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/drivers/timer.c.obj"
+  "J:/sipeed/k210/apu_test/lib/drivers/uart.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/drivers/uart.c.obj"
+  "J:/sipeed/k210/apu_test/lib/drivers/uarths.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/drivers/uarths.c.obj"
+  "J:/sipeed/k210/apu_test/lib/drivers/utils.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/drivers/utils.c.obj"
+  "J:/sipeed/k210/apu_test/lib/drivers/wdt.c" "J:/sipeed/k210/apu_test/build/lib/CMakeFiles/kendryte.dir/drivers/wdt.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CONFIG_LOG_COLORS"
+  "CONFIG_LOG_ENABLE"
+  "CONFIG_LOG_LEVEL=LOG_VERBOSE"
+  "DEBUG=1"
+  "LOG_KERNEL"
+  "LV_CONF_INCLUDE_SIMPLE"
+  "__riscv64"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../lib/bsp/include"
+  "../lib/drivers/include"
+  "../lib/utils/include"
+  "../src/apu_test"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
